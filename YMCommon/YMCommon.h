ff2a9abae9b6a6448bf96ab76bb2aa73b7f86903
Mccan <uhuacall@uhuacalls-Mac-mini.local>
2016-12-02 13:36:22 +0800

    inital

--- YMCommon/YMCommon.h
+++ YMCommon/YMCommon.h
@@ -10,4 +10,6 @@
 
 @interface YMCommon : NSObject
 
+-(void)myTestStaticLib;
+
 @end
--- YMCommon/YMCommon.m
+++ YMCommon/YMCommon.m
@@ -10,4 +10,9 @@
 
 @implementation YMCommon
 
+-(void)myTestStaticLib
+{
+    NSLog(@"testing myTest's static library!");
+}
+
 @end
