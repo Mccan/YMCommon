ac82c4db53cc27c394ac8963c840064751b38062
Mccan <uhuacall@uhuacalls-Mac-mini.local>
2016-12-02 13:32:34 +0800

    Initial Commit

---
+++ YMCommon/YMCommon.h
@@ -0,0 +1,13 @@
+//
+//  YMCommon.h
+//  YMCommon
+//
+//  Created by uhuacall on 2016/12/2.
+//  Copyright © 2016年 Macan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YMCommon : NSObject
+
+@end
---
+++ YMCommon/YMCommon.m
@@ -0,0 +1,13 @@
+//
+//  YMCommon.m
+//  YMCommon
+//
+//  Created by uhuacall on 2016/12/2.
+//  Copyright © 2016年 Macan. All rights reserved.
+//
+
+#import "YMCommon.h"
+
+@implementation YMCommon
+
+@end
